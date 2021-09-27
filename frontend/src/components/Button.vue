063801e949934873160efcd70d8200a027cef400
James Hawkins <jhawki2005@gmail.com>
2021-09-27 22:36:46 +0100

    some button stuff

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <button @click="$emit('click')" :class="styles">
+    <slot>Click me!</slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+    font-family: inherit;
+    font-size: initial;
+    border: 0;
+}
+
+.primary {
+    background-color: blue;
+    color: white;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    styles() {
+      const obj = {};
+      obj[this.type] = true;
+
+      return {
+        'button': true,
+        ...obj,
+      }
+    }
+  }
+};
+</script>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,15 +2,23 @@
     <div>
         <Header />
         <h1>Home :D</h1>
+        <Button @click="test()" :type="'primary'">Text</Button>
     </div>
 </template>
 
 <script>
 import Header from '../components/partials/Header.vue';
+import Button from '../components/Button.vue';
 
 export default {
     components: {
-        Header
+        Header,
+        Button
+    },
+    methods: {
+        test() {
+            alert('clicked');
+        }
     }
 }
 </script>
